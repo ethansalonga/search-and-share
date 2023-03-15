090aafb1cdfc9f04c66168a61067adef5545f3ed
ethansalonga <ethan@ethansalonga.dev>
2023-03-15 19:45:02 -0400

    Using Axios to fetch coordinates from entered address

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,35 @@
+import axios from "axios"
+
 const form = document.querySelector("form")!
 const addressInput = document.getElementById("address")! as HTMLInputElement
 
 const GOOGLE_API_KEY = "AIzaSyAE4mf-U-IfiaY-CXa7RX9Ud12qhhers1w"
-const searchString =
-  "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY"
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[]
+  status: "OK" | "ZERO_RESULTS"
+}
 
 function searchAddressHandler(event: Event) {
   event.preventDefault()
   const enteredAddress = addressInput.value
 
-  // send to Google's API
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!")
+      }
+      const coordinates = response.data.results[0].geometry.location
+    })
+    .catch((err) => {
+      alert(err.message)
+      console.log(err)
+    })
 }
 
 form.addEventListener("submit", searchAddressHandler)
