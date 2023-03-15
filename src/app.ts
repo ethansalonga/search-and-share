36b793b8fff9cbae98cdf72254d227a6bf96257c
ethansalonga <ethan@ethansalonga.dev>
2023-03-15 19:33:09 -0400

    Create Google API key

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 const form = document.querySelector("form")!
 const addressInput = document.getElementById("address")! as HTMLInputElement
 
+const GOOGLE_API_KEY = "AIzaSyAE4mf-U-IfiaY-CXa7RX9Ud12qhhers1w"
+const searchString =
+  "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY"
+
 function searchAddressHandler(event: Event) {
   event.preventDefault()
   const enteredAddress = addressInput.value
